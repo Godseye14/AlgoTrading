6f09e10681b4fba1dfe5f90cb85cb3e94dfdcc2e
Gautam Damle <gautamdamle12@gmail.com>
2023-07-12 11:37:44 +0530

    Add default input feature

--- stockAnalysis.py
+++ stockAnalysis.py
@@ -7,6 +7,12 @@ symbol = input("Enter symbol : ")
 start = input("Start Date : ")
 end = input("End Date : ")
 
+if len(symbol)==0 and len(start)==0 and len(end)==0:
+    print("User Input not provided correctly.. Taking Bajaj Finance as default script...")
+    symbol = "BAJFINANCE.NS"
+    start = "2003-05-26"
+    end = "2023-07-12"
+
 df = get_data(symbol,start,end)
 if len(df)!=0:
     close = get_close(df)
